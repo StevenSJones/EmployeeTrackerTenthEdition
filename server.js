eb98ac7cb469c3058a5fc0c7eaf46a233a040d3a
SS Jones <SSJones.code@gmail.com>
2020-09-06 18:14:55 -0700

    I added the connection and port to the js file as well as verified that the sql worked.

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 //importing my node modules
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employees"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
