ccbf86b6e95c6c260da940665c7be400c8dd57fe
SS Jones <SSJones.code@gmail.com>
2020-09-06 20:19:46 -0700

    I added the inquirer.prompt and the initial questions to the javascript file.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //importing my node modules
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,27 +14,36 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "",
-  database: "employees"
+  database: "employees",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   runSearch();
 });
 
-function runSearch(){
-    function runSearch() {
-        inquirer
-          .prompt({
-            name: "action",
-            type: "rawlist",
-            message: "What would you like to do?",
-            choices: [
-              "Find songs by artist",
-              "Find all artists who appear more than once",
-              "Find data within a specific range",
-              "Search for a specific song",
-              "Find artists with a top song and top album in the same year"
-            ]
-          })
-} 
+function runSearch() {
+  function runSearch() {
+    inquirer.prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By department",
+        "View All Employees By manager",
+        "Add An Employee",
+        "Remove An Employee",
+        "Update An Employee Role",
+        "Update An Employee Manager",
+        "View All Roles",
+        "Add A Role",
+        "Remove A Role",
+        "View All departments",
+        "Add A Department",
+        "Remove A Department",
+        "Quit"
+      ],
+    });
+  }
+}
