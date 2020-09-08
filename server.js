bae1f5e4d62aabdfcc48886cc2bcffe408d616cc
SS Jones <SSJones.code@gmail.com>
2020-09-07 19:35:51 -0700

    I removed some lines that seemed to be not needed from the javascript.

--- server.js
+++ server.js
@@ -2,17 +2,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-
 //connection for my server
 const connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: "root",
-
   // Your password
   password: "",
   database: "employees",
@@ -24,7 +20,6 @@ connection.connect(function (err) {
 });
 
 //my function definition for my init function
-
 function runSearch() {
   inquirer
     .prompt({
@@ -32,20 +27,10 @@ function runSearch() {
       type: "list",
       message: "What would you like to do?",
       choices: [
-        "View All Employees",
-        "View All Employees By department",
-        "View All Employees By manager",
-        "Add An Employee",
-        "Remove An Employee",
-        "Update An Employee Role",
-        "Update An Employee Manager",
-        "View All Roles",
-        "Add A Role",
-        "Remove A Role",
-        "View All departments",
-        "Add A Department",
-        "Remove A Department",
-        "Quit",
+        "View All Employees", "View All Employees By department", "View All Employees By manager",
+        "Add An Employee", "Remove An Employee", "Update An Employee Role", "Update An Employee Manager",
+        "View All Roles", "Add A Role", "Remove A Role", "View All departments", "Add A Department",
+        "Remove A Department", "Quit",
       ],
       //.then to handle the response
     })
